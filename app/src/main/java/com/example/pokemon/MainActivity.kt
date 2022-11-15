188a6ebf11961d509707c647358fc5f14a28ea41
usama_said <usamasaid142@gmail.com>
2022-11-15 07:13:46 +0200

    is valid

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -8,5 +8,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun isValidData(
+        serialmacinescan: String,
+        serialIMEIScan: String,
+        serialThemeScan: String,
+        traderNumber: String,
+        commit: String
+    ): Boolean {
+        var isvalid = true
 
+        if (serialmacinescan.trim().isNullOrEmpty()) {
+            binding.etScanserial.error = "required"
+            isvalid = false
+        } else {
+            binding.etScanserial.error = null
+            isvalid = true
+        }
+        if (serialIMEIScan.trim().isNullOrEmpty()) {
+            binding.etScanIMEI.error = "required"
+            isvalid = false
+        } else {
+            binding.etScanIMEI.error = null
+            isvalid = true
+        }
+        if (serialThemeScan.trim().isNullOrEmpty()) {
+            binding.etScantheme.error = "required"
+            isvalid = false
+        } else {
+            binding.etScantheme.error = null
+            isvalid = true
+        }
+        if (traderNumber.trim().isNullOrEmpty()) {
+            binding.etTrader.error = "required"
+            isvalid = false
+        } else {
+            binding.etTrader.error = null
+            isvalid = true
+        }
+        if (commit.trim().isNullOrEmpty()) {
+            binding.etCommit.error = "required"
+            isvalid = false
+        } else {
+            binding.etCommit.error = null
+            isvalid = true
+        }
+
+        return isvalid
+    }
 }
